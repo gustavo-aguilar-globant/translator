8bab439a9e9d6a63a5aa5b80b8d2c1d6594d6c7a
Gustavo Aguilar <gustavo.aguilar@globant.com>
2023-06-20 12:27:27 -0600

    fix bad encodings

--- translator.go
+++ translator.go
@@ -1,20 +1,48 @@
 package translator
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
-	"github.com/magiconair/properties"
+	"golang.org/x/text/encoding/charmap"
 )
 
 func ReadPropertiesFile(filePath string) (map[string]string, error) {
-	p, err := properties.LoadFile(filePath, properties.UTF8)
+	file, err := os.Open(filePath)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to open properties file: %w", err)
 	}
+	defer file.Close()
 
 	propertiesMap := make(map[string]string)
-	for _, key := range p.Keys() {
-		propertiesMap[key] = p.GetString(key, "")
+	scanner := bufio.NewScanner(file)
+	decoder := charmap.ISO8859_1.NewDecoder()
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "#") || strings.TrimSpace(line) == "" {
+			continue // Skip comments and empty lines
+		}
+
+		parts := strings.SplitN(line, "=", 2)
+		if len(parts) != 2 {
+			return nil, fmt.Errorf("invalid line format in properties file: %s", line)
+		}
+
+		key := strings.TrimSpace(parts[0])
+		value := strings.TrimSpace(parts[1])
+
+		decodedValue, err := decoder.String(value)
+		if err != nil {
+			return nil, fmt.Errorf("failed to decode property value: %w", err)
+		}
+
+		propertiesMap[key] = decodedValue
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("failed to read properties file: %w", err)
 	}
 
 	return propertiesMap, nil
